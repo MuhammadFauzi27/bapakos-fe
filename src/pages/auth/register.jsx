5c2f96356bbe03bb6ffb3ab8ab7fe4394baec21a
MuhammadFauzi27 <fauzym02@gmail.com>
2026-01-12 11:17:38 +0700

    commit success

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,11 +1,17 @@
 import {Link} from "react-router-dom";
 
-export const Login = () => {
+export const Register = () => {
   return (
     <div className="bg-white p-8 rounded shadow w-full max-w-md">
-      <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center">Register</h1>
 
       <form className="space-y-4">
+        <input
+          type="text"
+          placeholder="Nama"
+          className="w-full border p-2 rounded"
+        />
+
         <input
           type="email"
           placeholder="Email"
@@ -18,15 +24,15 @@ export const Login = () => {
           className="w-full border p-2 rounded"
         />
 
-        <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
-          Login
+        <button className="w-full bg-green-500 text-white py-2 rounded hover:bg-green-600">
+          Register
         </button>
       </form>
 
       <p className="text-sm text-center mt-4 text-gray-600">
-        Belum punya akun?{' '}
-        <Link to="/register" className="text-blue-500 hover:underline">
-          Register
+        Sudah punya akun?{' '}
+        <Link to="/" className="text-blue-500 hover:underline">
+          Login
         </Link>
       </p>
     </div>
--- src/routes/app-router.jsx
+++ src/routes/app-router.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Login} from "../pages/auth/login.jsx";
+import {Register} from "../pages/auth/register.jsx";
 
-export const AppRouter = async () => {
+export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </BrowserRouter>
   )
