4de345f9be7e25525cfeeced1fd47057e89d4b23
thejambangs <ramadhanasyikin@gmauil.com>
2026-01-14 16:02:53 +0700

    membuat halaman login & regis

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -2,33 +2,56 @@ import {Link} from "react-router-dom";
 
 export const Login = () => {
   return (
-    <div className="bg-white p-8 rounded shadow w-full max-w-md">
-      <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+    <div className="p-8 shadow w-screen min-h-screen flex">
+      <div className="flex size-auto rounded shadow">
+        {/* Gambar kiri */}
+        <div className="flex md:w-1/2 h-full hidden md:block overflow-hidden">
+          <img
+            src="/images/banner.png"
+            alt="Login Banner"
+            className="w-full h-full rounded object-cover"
+          />
+        </div>
 
-      <form className="space-y-4">
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full border p-2 rounded"
-        />
+        {/* Form kanan */}
+        <div className="w-full md:w-1/2 p-9 flex flex-col justify-center">
+          <div className="flex flex-col items-center mb-6">
+            <img
+              src="/images/logo.png"
+              alt="Logo"
+              className="size-20 mb-2"
+            />
+            <h1 className="text-2xl font-bold">Selamat Datang di BapaKos</h1>
+          </div>
 
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full border p-2 rounded"
-        />
+          <form className="space-y-4">
+            <p className="font-medium">Username</p>
+            <input
+              type="email"
+              placeholder="Arkan Saputra"
+              className="w-full border p-2 rounded"
+            />
 
-        <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
-          Login
-        </button>
-      </form>
+            <p className="font-medium">Password</p>
+            <input
+              type="password"
+              placeholder="********"
+              className="w-full border p-2 rounded"
+            />
 
-      <p className="text-sm text-center mt-4 text-gray-600">
-        Belum punya akun?{' '}
-        <Link to="/register" className="text-blue-500 hover:underline">
-          Register
-        </Link>
-      </p>
+            <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
+              Login
+            </button>
+          </form>
+
+          <p className="text-sm text-center mt-4 text-gray-600">
+            Belum punya akun?{" "}
+            <Link to="/register" className="text-blue-500 hover:underline">
+              Register
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -2,39 +2,64 @@ import {Link} from "react-router-dom";
 
 export const Register = () => {
   return (
-    <div className="bg-white p-8 rounded shadow w-full max-w-md">
-      <h1 className="text-2xl font-bold mb-6 text-center">Register</h1>
+    <div className="p-8 shadow w-screen min-h-screen flex">
+      <div className="flex size-auto rounded shadow">
+        {/* Gambar kiri */}
+        <div className="flex md:w-1/2 h-full hidden md:block overflow-hidden">
+          <img
+            src="/images/banner.png"
+            alt="Login Banner"
+            className="w-full h-full rounded object-cover"
+          />
+        </div>
 
-      <form className="space-y-4">
-        <input
-          type="text"
-          placeholder="Nama"
-          className="w-full border p-2 rounded"
-        />
+        {/* Form kanan */}
+        <div className="w-full md:w-1/2 p-9 flex flex-col justify-center">
+          <div className="flex flex-col items-center mb-6">
+            <img
+              src="/images/logo.png"
+              alt="Logo"
+              className="size-20 mb-2"
+            />
+            <h1 className="text-2xl font-bold">Buat Akun BapaKos</h1>
+          </div>
 
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full border p-2 rounded"
-        />
 
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full border p-2 rounded"
-        />
+          <form className="space-y-4">
+            <p className="font-medium">Username</p>
+            <input
+              type="text"
+              placeholder="Nur Intan"
+              className="w-full border p-2 rounded"
+            />
 
-        <button className="w-full bg-green-500 text-white py-2 rounded hover:bg-green-600">
-          Register
-        </button>
-      </form>
+            <p className="font-medium">Email</p>
+            <input
+              type="email"
+              placeholder="ramad****@gmail.com"
+              className="w-full border p-2 rounded"
+            />
 
-      <p className="text-sm text-center mt-4 text-gray-600">
-        Sudah punya akun?{' '}
-        <Link to="/" className="text-blue-500 hover:underline">
-          Login
-        </Link>
-      </p>
+            <p className="font-medium">Password</p>
+            <input
+              type="password"
+              placeholder="********"
+              className="w-full border p-2 rounded"
+            />
+
+            <button className="w-full bg-green-500 text-white py-2 rounded hover:bg-green-600">
+              Daftar
+            </button>
+          </form>
+
+          <p className="text-sm text-center mt-4 text-gray-600">
+            Sudah punya akun?{' '}
+            <Link to="/" className="text-blue-500 hover:underline">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
